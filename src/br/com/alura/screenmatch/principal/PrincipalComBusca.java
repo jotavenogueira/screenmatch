eeb0edac94fab3b51cef6af4abc38f3be3886302
João <jotavenog@gmail.com>
2026-04-08 04:49:40 -0300

    fazendo mecanismo de busca

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,13 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o filme para a busca: ");
+        var busca = leitura.nextLine();
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=ec39d554";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=ec39d554"))
+                .uri(URI.create(endereco))
                 .build();
 
         HttpResponse<String> response = client
