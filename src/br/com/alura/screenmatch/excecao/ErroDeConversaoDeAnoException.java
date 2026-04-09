bb4e610190dbd14c9dba961ffcc812ca99e73d00
João <jotavenog@gmail.com>
2026-04-08 23:16:10 -0300

    criando excecao

---
+++ src/br/com/alura/screenmatch/excecao/ErroDeConversaoDeAnoException.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch.excecao;
+
+public class ErroDeConversaoDeAnoException extends RuntimeException {
+
+    private String mensagem;
+
+    public ErroDeConversaoDeAnoException(String mensagem){
+        this.mensagem = mensagem;
+
+
+    }
+
+    public String getMensagem(){
+        return mensagem;
+    }
+}
